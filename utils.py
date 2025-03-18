217fbc2fa0cffc9f06ff174830e86904678365c0
pablocosta <pablo.botton.costa@gmail.com>
2025-03-18 13:28:38 -0300

    data analisys: done

--- utils.py
+++ utils.py
@@ -1,32 +1,38 @@
-from transformers import AutoTokenizer, AutoModel
-import torch
+
+from sentence_transformers import SentenceTransformer
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import normalize
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+import umap
+import hdbscan
 import numpy as np
-from sklearn.cluster import AgglomerativeClustering
-from sklearn.metrics.pairwise import cosine_similarity
+import matplotlib.pyplot as plt
 
+# 1. Gerar embeddings com LaBSE
+from sentence_transformers import SentenceTransformer
 
-# Carregando o modelo e o tokenizer
-model_name = "microsoft/mdeberta-v3-base"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModel.from_pretrained(model_name)
+model = SentenceTransformer('sentence-transformers/LaBSE')
+sentencas = [
+    "O gato está dormindo no sofá.",
+    "O cachorro está brincando com a bola.",
+    "O céu está azul hoje",
+    "A previsão é de chuva amanhã",
+    # Adicione suas sentenças aqui
+]
+embeddings = model.encode(sentencas)
 
-# Função que gera embeddings a partir das sentenças
-def gerar_embedding(sentenca):
-    tokens = tokenizer(sentenca, return_tensors='pt', padding=True, truncation=True)
-    with torch.no_grad():
-        outputs = model(**tokens)
-    embedding = outputs.last_hidden_state.mean(dim=1)
-    return embedding.numpy()
+# 2. Redução de dimensionalidade (PCA + UMAP)
+pca = umap.UMAP(n_components=50, n_neighbors=2, metric='cosine', random_state=42)
+embeddings_pca = pca.fit_transform(embeddings)
 
-# Gerar embeddings para um conjunto de sentenças
-def gerar_embeddings_conjunto(conjunto_sentencas):
-    return np.vstack([gerar_embedding(s) for s in conjunto_sentencas])
-
-# Agrupamento de sentenças utilizando clustering hierárquico
-def agrupar_sentencas(conjunto_sentencas, n_clusters):
-    embeddings = gerar_embeddings_conjunto(conjunto_sentencas)
-    matriz_similaridade = cosine_similarity(embeddings)
-
-    clustering = AgglomerativeClustering(n_clusters=n_clusters, affinity='precomputed', linkage='average')
-    agrupamentos = clustering.fit_predict(1 - matriz_similaridade)
-    return agrupamentos
+reducer = umap.UMAP(n_components=5, n_neighbors=2, metric='cosine', random_state=42)
+embeddings_umap = reducer.fit_transform(embeddings_pca)
+# Aqui demonstrado o método do cotovelo:
+inertia = []
+K_range = range(2, 5)
+for k in K_range:
+    km = KMeans(n_clusters=k, random_state=42)
+    km_clusters = km.fit(embeddings_pca)
+    inertia.append(km.inertia_)
+plt.plot(K_range, inertia)
